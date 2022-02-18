fdacdea1b6a700159684d016564beb1f2f281042
Ndeta Innocent <you@example.com>
2022-02-18 23:03:48 +0200

    using macros

--- intro_to_rust_02/src/main.rs
+++ intro_to_rust_02/src/main.rs
@@ -1,7 +1,12 @@
 use std::env;
+use std::fs;
 fn main() {
     let mut arguments = env::args().skip(1);
     let key = arguments.next().unwrap();
     let value = arguments.next().unwrap();
     println!("the key is : {} and the value is {}", key, value);
+
+    // Writing to a file
+    let contents = format!("{}\t{}\n", &key, &value);
+    fs::write("kv.db", contents);
 }
