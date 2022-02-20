49d4b749eb809513d56894012dea4d3c47630881
Ndeta Innocent <you@example.com>
2022-02-20 04:52:27 +0200

    introducing errors and Hashmaps

--- intro_to_rust_02/src/main.rs
+++ intro_to_rust_02/src/main.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
+use std::io;
 
 fn main() {
     let mut arguments = env::args().skip(1);
@@ -10,6 +12,29 @@ fn main() {
     // Writing to a file
     let contents = format!("{}\t{}\n", &key, &value);
     fs::write("kv.db", contents).unwrap();
+    let database = Database::new().expect("Database new crashed ");
 }
-struct Database {}
-impl Database {}
+struct Database {
+    map: HashMap<String, String>,
+}
+impl Database {
+    fn new() -> Result<Database, io::Error> {
+        //read the kv.db file
+        let contents = match fs::read_to_string("kv.db") {
+            Ok(c) => c,
+            Err(error) => {
+                return Err(error);
+            }
+        };
+        for line in contents.lines() {
+            let mut chunks = line.splitn(2, '\t');
+            let key = chunks.next().expect("No key");
+            let value = chunks.next().expect("No value");
+        }
+        //parse string
+        //populate map
+        Ok(Database {
+            map: HashMap::new(),
+        })
+    }
+}
